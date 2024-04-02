fa6df52a5f50e2209974ff080b0ee1f3942308e8
seohyeonwook <skut01541@gmail.com>
2024-04-02 20:59:51 +0900

    Remove : Dto readme삭제

--- src/main/java/com/novelidea/gumeonggage/controller/AuthController.java
+++ src/main/java/com/novelidea/gumeonggage/controller/AuthController.java
@@ -1,4 +1,19 @@
 package com.novelidea.gumeonggage.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")//공통 상위 주소
 public class AuthController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@Valid @RequestBody) {
+        return ResponseEntity.created(null).body(true);
+    }
 }
---
+++ src/main/java/com/novelidea/gumeonggage/dto/SignupReqDto.java
@@ -0,0 +1,4 @@
+package com.novelidea.gumeonggage.dto;
+
+public class SignupReqDto {
+}
