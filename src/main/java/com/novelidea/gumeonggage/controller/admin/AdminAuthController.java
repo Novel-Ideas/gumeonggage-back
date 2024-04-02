0b9d7465ef9f146c194a39d42ce5355d921e575f
dongyoon7212 <dongyoon7212@naver.com>
2024-04-03 00:17:12 +0900

    Feat: AdminAuthController 추가, 메소드 추가

---
+++ src/main/java/com/novelidea/gumeonggage/controller/admin/AdminAuthController.java
@@ -0,0 +1,33 @@
+package com.novelidea.gumeonggage.controller.admin;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/auth")
+public class AdminAuthController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> adminAuthSignup() {
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @GetMapping("/signin")
+    public ResponseEntity<?> adminAuthSignin() {
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @PutMapping("/edit/password")
+    public ResponseEntity<?> adminAuthEditPassword() {
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserAuth() {
+
+        return ResponseEntity.ok().body(null);
+    }
+}
