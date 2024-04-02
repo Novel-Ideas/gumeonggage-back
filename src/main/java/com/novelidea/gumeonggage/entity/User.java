cd80183b6c0f23d4efe3006f9fef19f1b124279e
seohyeonwook <skut01541@gmail.com>
2024-04-02 21:22:43 +0900

    Remove : entity readme삭제

--- src/main/java/com/novelidea/gumeonggage/entity/User.java
+++ src/main/java/com/novelidea/gumeonggage/entity/User.java
@@ -1,4 +1,20 @@
 package com.novelidea.gumeonggage.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    private int UserId;
+    private String phone;
+    private LocalDate createDate;
+    private LocalDate updateDate;
+
 }
