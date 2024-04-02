a0546fb68563b09f750299f3270da2cda5c5bb63
dongyoon7212 <dongyoon7212@naver.com>
2024-04-02 11:11:03 +0900

    init: first commit

---
+++ src/main/java/com/novelidea/gumeonggage/GumeonggageApplication.java
@@ -0,0 +1,13 @@
+package com.novelidea.gumeonggage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumeonggageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumeonggageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novelidea/gumeonggage/GumeonggageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novelidea.gumeonggage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumeonggageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
