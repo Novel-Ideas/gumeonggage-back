1378d026d6bcd1516157cdcdcac28cf3e0a05cbc
ez0603 <dlwldjs3132@naver.com>
2024-04-03 16:14:01 +0900

    Feat: repository FeedbackMapper 메소드 추가

--- src/main/java/com/novelidea/gumeonggage/repository/FeedbackMapper.java
+++ src/main/java/com/novelidea/gumeonggage/repository/FeedbackMapper.java
@@ -2,8 +2,17 @@ package com.novelidea.gumeonggage.repository;
 
 import com.novelidea.gumeonggage.entity.user.Feedback;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface FeedbackMapper {
     public int saveFeedback(Feedback feedback);
+    public List<Feedback> findFeedback(
+            @Param("orderListId") int orderListId,
+            @Param("answer1") int answer1,
+            @Param("answer2") int answer2,
+            @Param("answer3") int answer3
+    );
 }
