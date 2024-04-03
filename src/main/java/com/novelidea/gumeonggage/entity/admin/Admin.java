72051eb4ee5fd238f02b46644e29ca8a2bc1f4fa
seohyeonwook <skut01541@gmail.com>
2024-04-03 15:23:33 +0900

    Feat : dto AdminSignupReqDto 클래스 생성

--- src/main/java/com/novelidea/gumeonggage/entity/admin/Admin.java
+++ src/main/java/com/novelidea/gumeonggage/entity/admin/Admin.java
@@ -1,4 +1,23 @@
 package com.novelidea.gumeonggage.entity.admin;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Admin {
+    private int adminId;
+    private int roleId;
+    private String adminName;
+    private String adminPassword;
+    private String email;
+    private LocalDate createDate;
+    private LocalDate updateDate;
 }
