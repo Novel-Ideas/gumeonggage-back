fae86775f543b95b8dfa62bd3fe02251b2031993
ez0603 <dlwldjs3132@naver.com>
2024-04-03 14:24:54 +0900

    Feat: FeedbackServi

--- src/main/java/com/novelidea/gumeonggage/controller/user/UserMenuController.java
+++ src/main/java/com/novelidea/gumeonggage/controller/user/UserMenuController.java
@@ -1,19 +1,27 @@
 package com.novelidea.gumeonggage.controller.user;
 
 import com.novelidea.gumeonggage.dto.user.FeedbackReqDto;
+import com.novelidea.gumeonggage.service.user.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/menu")
 public class UserMenuController {
+    @Autowired
+    private FeedbackService feedbackService;
+
 
     @PostMapping("/order/feedback")
-    public ResponseEntity<?> addFeedback(@RequestBody FeedbackReqDto feedbackReqDto) {
-        System.out.println("/order/feedback");
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<?> saveFeedback(@Valid @RequestBody FeedbackReqDto feedbackReqDto, BindingResult bindingResult) {
+        feedbackService.saveFeedback(feedbackReqDto);
+        return ResponseEntity.created(null).body(true);
     }
 }
--- src/main/java/com/novelidea/gumeonggage/dto/user/FeedbackReqDto.java
+++ src/main/java/com/novelidea/gumeonggage/dto/user/FeedbackReqDto.java
@@ -1,5 +1,6 @@
 package com.novelidea.gumeonggage.dto.user;
 
+import com.novelidea.gumeonggage.entity.user.Feedback;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,13 @@ public class FeedbackReqDto {
     private int answer1;
     private int answer2;
     private int answer3;
+
+    public Feedback toEntity() {
+        return Feedback.builder()
+                .orderListId(orderListId)
+                .answer1(answer1)
+                .answer2(answer2)
+                .answer3(answer3)
+                .build();
+    }
 }
