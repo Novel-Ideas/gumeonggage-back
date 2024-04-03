a6854524acadac4fb17a63b5cbd8b2e576a6a773
ez0603 <dlwldjs3132@naver.com>
2024-04-03 09:47:16 +0900

    Feat: UserMenuController 추가 및 피드백 메소드 추가

---
+++ src/main/java/com/novelidea/gumeonggage/controller/user/UserMenuController.java
@@ -0,0 +1,16 @@
+package com.novelidea.gumeonggage.controller.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
+public class UserMenuController {
+
+    @PostMapping("/menu/order/feedback")
+    public ResponseEntity<?> addFeedback() {
+        return ResponseEntity.ok(null);
+    }
+}
