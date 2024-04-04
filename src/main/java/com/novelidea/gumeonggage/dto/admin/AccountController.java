0ce7c3470043d05cbad060397961224d27a4e092
seohyeonwook <skut01541@gmail.com>
2024-04-04 17:42:03 +0900

    Feat : 보안/ 예외/ 토큰/ 사용자 회원가입/ 관리자 회원가입, 로그인 작업

--- src/main/java/com/novelidea/gumeonggage/dto/admin/AccountController.java
+++ src/main/java/com/novelidea/gumeonggage/dto/admin/AccountController.java
@@ -1,4 +1,12 @@
 package com.novelidea.gumeonggage.dto.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+
+
 }
