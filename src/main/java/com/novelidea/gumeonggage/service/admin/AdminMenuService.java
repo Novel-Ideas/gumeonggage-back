046031f23fc1fd59c3c84a39df20449c6fa454e5
ez0603 <dlwldjs3132@naver.com>
2024-04-04 10:48:40 +0900

    Feat: 메뉴 추가 관리자 모드 완료

--- src/main/java/com/novelidea/gumeonggage/service/admin/AdminMenuService.java
+++ src/main/java/com/novelidea/gumeonggage/service/admin/AdminMenuService.java
@@ -1,13 +1,20 @@
 package com.novelidea.gumeonggage.service.admin;
 
+import com.novelidea.gumeonggage.dto.admin.AdminRegisterMenuReqDto;
 import com.novelidea.gumeonggage.repository.MenuMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdminMenuService {
 
     @Autowired
     private MenuMapper menuMapper;
+
+    @Transactional(rollbackFor = Exception.class)
+    public int saveMenu(AdminRegisterMenuReqDto adminRegisterMenuReqDto) {
+        return menuMapper.saveMenu((adminRegisterMenuReqDto.toEntity()));
+    }
 
 }
